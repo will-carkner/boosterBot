350381d0a29e3d0f6d5edb99e0c00d26b630d648
ChickenTaxi <willcarkner2006@gmail.com>
2021-05-17 14:11:40 +0100

    made it able to do infinite items

--- apiTesting.py
+++ apiTesting.py
@@ -4,13 +4,10 @@ import json
 import sys
 
 url = 'https://api.hypixel.net/skyblock/auctions'
-item1 = 'Kismet Feather'
-item2 = 'God Potion'
-item3 = 'Kat Flower'
-
-aucts1 = []
-aucts2 = []
-aucts3 = []
+items = ['Kismet Feather',
+         'God Potion',
+         'Kat Flower']
+aucts = []
 
 req = requests.get(url)
 nPages = json.loads(req.text)['totalPages']
@@ -25,28 +22,20 @@ def getPage(p):
 with concurrent.futures.ThreadPoolExecutor() as executor:
     results = executor.map(getPage, range(nPages))
 
-aucts1 = [item1 for sublist in results for item1 in sublist]
-aucts2 = [item2 for sublist in results for item2 in sublist]
-aucts3 = [item3 for sublist in results for item3 in sublist]
+aucts = [items for sublist in results for items in sublist]
 
 
 def sBid(e):
     return e['starting_bid']
 
 
-good1 = [x for x in aucts1 if x['item_name'] == item1]
-good2 = [x for x in aucts2 if x['item_name'] == item2]
-good3 = [x for x in aucts3 if x['item_name'] == item3]
+binned = [a for a in aucts if 'bin' in a and a['bin']]
 
-out1 = [a for a in good1 if 'bin' in a and a['bin']]
-out2 = [a for a in good2 if 'bin' in a and a['bin']]
-out3 = [a for a in good3 if 'bin' in a and a['bin']]
+#startingBits = int(input("How many bits do you have?: "))
 
-out1.sort(key=sBid)
-out2.sort(key=sBid)
-out3.sort(key=sBid)
-
-print([a['starting_bid'] for a in out1[:3]], end='')
-print([a['starting_bid'] for a in out2[:3]], end='')
-print([a['starting_bid'] for a in out3[:3]], end='')
+for item in items:
+    out = [a for a in binned if a['item_name'] == item]
+    out.sort(key=sBid)
+    prices = [a['starting_bid'] for a in out[:3]]
+    print(round(sum(prices) / len(prices)))
 sys.stdout.flush()
